1be4e522fe7ac0b1592363c0f37aa7a70e745df9
linyows <linyows@gmail.com>
2016-10-12 18:30:38 +0900

    typo

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,7 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # for sqlite3 use in memory
-load Rails.root.join('/db/schema.rb') if ENV['CI']
+load Rails.root.join('db/schema.rb') if ENV['CI']
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
