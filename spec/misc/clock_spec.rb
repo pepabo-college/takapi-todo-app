e5dd6dd245e5fb74a3a6c6652271e9c3566279cb
ligtnrcat <nakamura.k@pepabo.com>
2016-10-14 15:41:29 +0900

    Modify clock test content

--- spec/misc/clock_spec.rb
+++ spec/misc/clock_spec.rb
@@ -22,7 +22,7 @@ describe "clock.rb" do
 
   it "" do
     expect(@for_test).to receive(:_show).with("コンテンツ").once
-    allow(RemindMailer).to receive(:sendmail).and_return(true)
+    allow(RemindMailer).to receive(:deliver).and_return(true)
     @for_test.reminder_mail
   end
 
