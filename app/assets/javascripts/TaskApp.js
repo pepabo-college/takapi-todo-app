969ed3b69eeb0e2dd832bdae29146674b4592d24
yinm <yinm@pepabo.com>
2016-10-14 16:26:41 +0900

    Reactでのレンダリング確認
    WEB+DB PRESSのP33の「ToDoの一覧表示とToDoの追加」の手前まで完了

---
+++ app/assets/javascripts/TaskApp.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class TaskApp extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="TaskApp">
+        Hello, Web Development World!
+      </div>
+    );
+  }
+}
