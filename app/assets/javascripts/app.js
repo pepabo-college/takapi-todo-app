cc72ad7330fed52a06b0af2c07e2660f28edafb5
litencatt <ncl0709@gmail.com>
2016-10-16 16:55:54 +0900

    Todoの一覧表示と追加機能の追加
    P-35 ToDoの更新機能を追加の前まで実装

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -4,7 +4,7 @@ import TaskApp from "./TaskApp";
 
 $(function() {
   ReactDOM.render(
-    <TaskApp />,
+    <TaskApp url="/tasks" pollInterval={2000} />,
     document.getElementById('container')
   );
 });
