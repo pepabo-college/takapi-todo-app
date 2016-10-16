2c61db322bb11e8cf9bc8b2f30f3d62bf25f2202
ligtnrcat <ncl0709@gmail.com>
2016-10-16 12:37:53 +0900

    .gitignoreへのディレクトリ追加 ダイジェスト認証処理のコメントアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   USERS = { ENV['USER'] => ENV['PASS'] }
 
-  before_action :digest_auth
+#  before_action :digest_auth
 
   def digest_auth
     authenticate_or_request_with_http_digest do |user|
