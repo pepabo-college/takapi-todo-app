7e1ea74f2e782b128d25acf318cf88314acd8ef8
Uchio KONDO <udzura@pepabo.com>
2016-10-17 11:04:26 +0900

    前準備をbeforeに押し込めた

--- spec/misc/clock_spec.rb
+++ spec/misc/clock_spec.rb
@@ -15,6 +15,11 @@ describe "clock.rb" do
        status: "todo",
        duedate: Date.yesterday.to_s
     ).save
+
+    @mailer = double(RemindMailer, deliver: true)
+
+    allow(RemindMailer).to receive(:sendmail).and_return(@mailer)
+    allow(@for_test).to receive(:_show).with("コンテンツ")
   end
 
   after do
@@ -23,18 +28,12 @@ describe "clock.rb" do
 
   it "_showでメッセージを受け取ること" do
     expect(@for_test).to receive(:_show).with("コンテンツ").once
-    mailer = double(RemindMailer, deliver: true)
-
-    allow(RemindMailer).to receive(:sendmail).and_return(mailer)
     @for_test.reminder_mail
   end
 
   it "通知メールが送られること" do
-    allow(@for_test).to receive(:_show).with("コンテンツ")
-
-    mailer = double(RemindMailer, deliver: true)
-    expect(mailer).to receive(:deliver).once
-    expect(RemindMailer).to receive(:sendmail).and_return(mailer)
+    expect(@mailer).to receive(:deliver).once
+    expect(RemindMailer).to receive(:sendmail).and_return(@mailer)
 
     @for_test.reminder_mail
   end
