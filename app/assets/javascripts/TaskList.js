8b2f7d2e7459826ff5dae5c50b5c8f7c5073b040
ligtnrcat <nakamura.k@pepabo.com>
2016-10-18 11:16:07 +0900

    タスクへのメモ機能の追加

--- app/assets/javascripts/TaskList.js
+++ app/assets/javascripts/TaskList.js
@@ -5,8 +5,7 @@ export default class TaskList extends React.Component {
   render() {
     var tasks = this.props.data.map((task) => {
       return (
-        <Task key={task.id} id={task.id}
-              content={task.content} status={task.status} onTaskUpdate={this.props.onTaskUpdate} >
+        <Task key={task.id} id={task.id} content={task.content} status={task.status} memo={task.memo} onTaskUpdate={this.props.onTaskUpdate} >
         </Task>
       );
     });
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -50,6 +50,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:content, :status)
+    params.require(:task).permit(:content, :status, :memo)
   end
 end
